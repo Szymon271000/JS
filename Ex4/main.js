175ef63ac01fda3a081a82460480096ffbad651f
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-11-19 11:40:36 +0100

    Added saving to local storage

--- Ex4/main.js
+++ Ex4/main.js
@@ -1,6 +1,11 @@
 const addBox = document.querySelector(".add-box");
 const popupBox = document.querySelector(".popup-box");
 const closeIcon = document.querySelector("header i");
+const titleTag = document.querySelector("input");
+const descTag = document.querySelector("textarea");
+const addBtn = popupBox.querySelector("button");
+const months = ["January", "February", "March", "April", "May", "June", 
+"July", "August", "September", "October", "November", "December"];
 
 addBox.addEventListener("click", () => {
     popupBox.classList.add("show");
@@ -8,4 +13,30 @@ addBox.addEventListener("click", () => {
 
 closeIcon.addEventListener("click", () => {
     popupBox.classList.remove("show");
+});
+
+
+closeIcon.addEventListener("click", () => {
+    popupBox.classList.remove("show");
+});
+
+addBtn.addEventListener("click", e => {
+    e.preventDefault();
+    let noteTitle = titleTag.value;
+    let noteDescription = descTag.value;
+    if (noteTitle || noteDescription) {
+        let dateObj = new Date();
+        let month = months[dateObj.getMonth()];
+        let day = dateObj.getDay();
+        let year = dateObj.getFullYear();
+        let noteInfo = {
+            title: noteTitle,
+            description: noteDescription,
+            date: month, day, year
+        }
+
+        const notes = [];
+        notes.push(noteInfo);
+        localStorage.setItem("notes", JSON.stringify(notes));
+    }
 });
