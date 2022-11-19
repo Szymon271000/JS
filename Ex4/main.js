650e83af8d2db8e2f326e6046364b9cd97b77c91
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-11-19 11:23:13 +0100

    Added eventListener for closing form

--- Ex4/main.js
+++ Ex4/main.js
@@ -1,6 +1,11 @@
-const addBox = document.querySelector(".add-box"),
-popupBox = document.querySelector(".popup-box");
+const addBox = document.querySelector(".add-box");
+const popupBox = document.querySelector(".popup-box");
+const closeIcon = document.querySelector("header i");
 
 addBox.addEventListener("click", () => {
     popupBox.classList.add("show");
-})
+});
+
+closeIcon.addEventListener("click", () => {
+    popupBox.classList.remove("show");
+});
