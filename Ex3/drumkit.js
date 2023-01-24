7af3abede477680dd010417f545bf70d36652b8e
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2023-01-24 20:41:31 +0100

    Updated drumkit

--- Ex3/drumkit.js
+++ Ex3/drumkit.js
@@ -1,38 +1,85 @@
-let numberOfButtons = document.querySelectorAll(".button").length;
-for (var j = 0; j < numberOfButtons; j++) {
-  
-    document.querySelectorAll(".button")[j]
-      .addEventListener("click", function() {
-        let buttonStyle = this.innerHTML;
-        sound(buttonStyle);
-    });
-  }
+const WHITE_KEYS = ['q','w','e','r'];
+const BLACK_KEYS = ['a','s','d'];
 
-  document.addEventListener("keypress", function(event) {
-    sound(event.key);
-  });
+const recordButton = document.querySelector('.record-button');
+const keys = document.querySelectorAll(".key");
+const whiteKeys = document.querySelectorAll(".key.white");
+const blackKeys = document.querySelectorAll(".key.black");
 
-  function sound(key) {
-    switch (key) {
-      case "w":
-        var sound1 = new Audio("sounds/boom.wav");
-        sound1.play();
-        break;
-    
-      case "a":
-        var sound2 = new Audio("sounds/clap.wav");
-        sound2.play();
-        break;
-    
-      case "s":
-        var sound3 = new Audio("sounds/tom.wav");
-        sound3.play();
-        break;
-    
-      case "d":
-        var sound4 = new Audio("sounds/tink.wav");
-        sound4.play();
-        break;
+const keyMap = [...keys].reduce((map, key) => {
+    map[key.dataset.note] = key;
+    return map;
+}, {})
+
+//keyMap[boom]
+// {boom: key[0], clap: key[1]}
+
+let recordingStartTime;
+let songNotes;
+recordButton.addEventListener('click', toggleRecording);
+
+function toggleRecording() {
+    recordButton.classList.toggle('active');
+    if (isRecording()) {
+        startRecording();
+    }
+    else {
+        stopRecording();
     }
 }
 
+function isRecording() {
+    return recordButton != null && recordButton.classList.contains('active');
+}
+
+function startRecording() {
+    recordingStartTime = Date.now();
+    songNotes = [];
+}
+
+function stopRecording() {
+    playSong();
+}
+
+function playSong() {
+    if (songNotes.length === 0 ) { 
+        return;
+    }
+
+    songNotes.forEach(note => {
+        setTimeout(() => {
+            playNote(keyMap[note.key]);
+        }, note.startTime)
+    });
+}
+
+document.addEventListener('keydown', e => {
+    if (e.repeat) {
+        return;
+    }
+    const key = e.key;
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+    const blackKeyIndex = BLACK_KEYS.indexOf(key);
+    if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex]);
+    if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex]);
+})
+
+function playNote(key) {
+    if (isRecording()) {
+        recordNote(key.dataset.note);
+    }
+    const noteAudio = document.getElementById(key.dataset.note);
+    noteAudio.currenTime = 0;
+    noteAudio.play();
+    key.classList.add('active');
+    noteAudio.addEventListener('ended', () => {
+        key.classList.remove('active');
+    })
+}
+
+function recordNote(note) {
+    songNotes.push({
+        key: note,
+        startTime: Date.now() - recordingStartTime
+    });
+}
